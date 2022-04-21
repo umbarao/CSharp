e0ae667513d04cc3c4ab7d51a2f326f43e39718f
Gabriel Luccas <gabrelluccas.developer@gmail.com>
2022-04-21 01:22:15 -0300

    C#

--- Aulas/Aula19/exercicio3/EX3/Program.cs
+++ Aulas/Aula19/exercicio3/EX3/Program.cs
@@ -4,6 +4,7 @@ class Exercicio3
     static void Main()
     {
         string[] nomes = new string[3]; int[] n1 = new int[3]; int[] n2 = new int[3]; int[] soma = new int[3];
+        string[] decisao = new string[3];
 
         for(int i = 0; i < 3; i++)
         {
@@ -15,11 +16,22 @@ class Exercicio3
             n2[i] = int.Parse(Console.ReadLine());
 
             soma[i] = (n1[i] + n2[i]) / 2;
+
+            if (soma[i] >= 7)
+            {
+                decisao[i] = "Aprovado!";
+            }
+            else
+            {
+                decisao[i] = "Reprovado!";
+            }
         }
+        
+        Console.Clear();
 
         for(int i = 0; i < 3; i++)
         {
-            Console.WriteLine("--- NOTA DO ALUNO ---\nAluno: {0}\nMédia do Aluno: {1}\n", nomes[i], soma[i]);
+            Console.WriteLine("--- NOTA DO ALUNO ---\nAluno: {0}\nMédia do Aluno: {1}\n{2}\n", nomes[i], soma[i], decisao[i]);
         }
     }
 }
