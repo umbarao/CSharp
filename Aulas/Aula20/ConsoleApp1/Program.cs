8d70f9fb1bd5d1cb74863c61afbd684857c6b4a1
Gabriel Luccas <gabrelluccas.developer@gmail.com>
2022-04-21 23:44:11 -0300

    C#

---
+++ Aulas/Aula20/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Aula20
+{
+    static void Main()
+    {
+        int[] num = new int[10];
+        int i = 0;
+
+        while (i<num.Length)
+        {
+            num[i] = 0;
+            Console.WriteLine("@umbarao");
+            i++;
+        }
+        Console.WriteLine("Fim do Loop");
+    }
+}
---
+++ Aulas/Aula20/ex1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Ex1
+{
+    static void Main()
+    {
+        int[] num = new int[10];
+
+        int i = 10;
+        
+        while(i > 0){ // 10 > 0 sim 9>0 sim ...... 0>0 nao o codigo para
+            Console.WriteLine(i);
+
+            i--;
+        }
+
+        Console.WriteLine("_________________");
+
+        int[] num1 = new int[10]; // vetor num com tamanho 10
+        int u = num1.Length; ; // variavel u recebeu o valor do tamanho tamanho do vetor num 
+     
+        while(u > 0) // u > 0; como foi declarado que u recebe valor  do tamanho do vetor a condição se aplica enquanto 10 for > 0, o laço se repete
+        {
+            Console.WriteLine(u); // vai printar na tela o valor de u até que o laço pare
+
+            u--; // decremento - o valor de u vai diminuindo até que a condição seja satisfeita 0>0 'false'
+        }
+    }
+}
