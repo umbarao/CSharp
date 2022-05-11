d4831eb64853d435590daa82a279fbfa041a6c5d
Gabriel Luccas <gabrelluccas.developer@gmail.com>
2022-05-10 22:33:05 -0300

    C#

---
+++ Aulas/Aula25/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Aula25
+{
+    static void Main()
+    {
+        int num = 0;
+        int num2 = 0;
+        int num4 = 0;
+        int chamar = 0;
+
+
+        Console.WriteLine("Digite um valor: ");
+        chamar = int.Parse(Console.ReadLine());
+        multiplicar(ref chamar);
+        Console.WriteLine("O Resultado da Multiplicação é: {0}", chamar);
+
+        Console.WriteLine("Digite um valor: ");
+        num = int.Parse(Console.ReadLine());
+        dobrar(ref num);
+        Console.WriteLine("O Resultado do Dobro é: {0}", num);
+
+        Console.WriteLine("Digite um valor: ");
+        num2 = int.Parse(Console.ReadLine());
+        somar(ref num2);
+        Console.WriteLine("O Resultado da Soma é: {0}", num2);
+
+        Console.WriteLine("Digite um valor: ");
+        num4 = int.Parse(Console.ReadLine());
+        divisao(ref num4);
+        Console.WriteLine(num4);
+        Console.WriteLine("O Resultado da Divisão é: {0}", num4);
+
+    }
+    static void dobrar(ref int valor)
+    {
+        valor *= 2;
+    }
+
+    static void somar(ref int valor1)
+    {
+        valor1 = valor1 + valor1;
+    }
+    
+    static void multiplicar(ref int valor2)
+    {
+        valor2 = valor2 * valor2;
+    }
+
+    static void divisao(ref int valor3)
+    {
+        valor3 = valor3 / valor3;
+    }
+}
---
+++ Aulas/Aula26/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+class Aula26
+{
+    static void Main()
+    {
+        int divid, divis, quoc, rest;
+        divid = 0;
+        divis = 0;
+
+        Console.WriteLine("***CALCULO DE DIVISÂO***");
+
+        Console.Write("Digite o Dividendo: ");
+        divid = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o Divisor: ");
+        divis = int.Parse(Console.ReadLine());
+
+        quoc = divide(divid, divis, out rest);
+
+        Console.WriteLine("{0}/{1}: quociente = {2} e resto = {3}", divid, divis, quoc, rest);
+    }
+    
+    static int divide(int dividendo, int divisor, out int resto)
+    {
+        int quociente;
+        quociente = dividendo / divisor;
+        resto = dividendo % divisor;
+        return quociente;
+
+    }
+}
