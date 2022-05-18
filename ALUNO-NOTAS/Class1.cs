2fb5ef2a40cfe1745f3450194d351b1d5e59d826
Gabriel Luccas <gabrelluccas.developer@gmail.com>
2022-05-18 00:54:09 -0300

    C#

---
+++ ALUNO-NOTAS/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace NOTAS
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
+
---
+++ ALUNO-NOTAS/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace NOTAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while(opcaoUsuario.ToUpper() != "X") 
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do Aluno: ");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do Aluno: ");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal!");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+
+                    case "2":
+                        foreach (var a in alunos)
+                        {
+                            if(!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+                        break;
+
+                    case "3":
+                        decimal notaTotal = 0;
+                        var numAlunos = 0;
+                        for(int i = 0; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                numAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notaTotal / numAlunos;
+                        Console.WriteLine($"MÈDIA GERAL: {mediaGeral}");
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("INFORME A OPÇÃO DESEJADA");
+            Console.WriteLine("1 - Inserir um novo aluno\n2 - Listar alunos\n3 - Calcular média geral\nX - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+}
